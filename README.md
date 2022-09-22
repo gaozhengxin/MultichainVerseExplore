223305f68a1492ed34ac4a30cd83decfc3700bb6
gaozhengxin <zhengxingao@live.cn>
2022-09-22 18:56:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+GAS_REPORT=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+# Testnet Address
+Fantom testnet
+0x92BA974d376609E403ce8cf779d2F464EBC5E018
+
+BSC testnet
+0xE1Db687Ee2f5Ec0e0Be2f8A1155E8707893De238
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const MVE = await hre.ethers.getContractFactory("MultichainVerseExplore");
+  const mve = await MVE.deploy();
+
+  await mve.deployed();
+
+  console.log(
+    `Deployed to ${mve.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
